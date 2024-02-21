7a70fa618d92d801190e089cbd79e1481b86a8b8
orozbai <altynbekov.bai@gmail.com>
2024-02-21 23:38:14 +0600

    git init

---
+++ src/main/java/org/example/crmschool/CrmSchoolApplication.java
@@ -0,0 +1,13 @@
+package org.example.crmschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmSchoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/crmschool/CrmSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.crmschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
